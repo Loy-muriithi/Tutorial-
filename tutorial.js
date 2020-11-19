6ab0cca1dadb12ba600ea0c0c943ccd54c18ebd2
loy muriithi <loymur8gail.com>
2020-11-20 00:35:51 +0300

    commit

--- tutorial.js
+++
@@ -1,9 +0,0 @@
-const sum = (num1,num2) => num1 + num2;
-const PI = 3.14;
-class SomeMathObject{
-    constructor(){
-        console.log('Object created');
-    }
-}
-module.exports = {sum : sum, pI : PI, SomeMathObject: SomeMathObject}
-
