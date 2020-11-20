54061114f4f17745c6ae38176f5542f6c4e6df82
loy muriithi <loymur8gail.com>
2020-11-21 02:43:00 +0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.write('Hello word from nodejs');
-        res.end();
-    }
-    else{
-        res.write('using some other domain');
-        res.end();
-    }
-    res.write('Hello World from nodejs');
-    res.end();
-});
-
-server. listen('3000');
+const 
