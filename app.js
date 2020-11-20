8a3337c4fbb742ab0ac42f25ce157b55a87b247d
loy muriithi <loymur8gail.com>
2020-11-20 22:37:53 +0300

    commit

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ let num1 = Math.floor((Math.random() * 10) + 1);
 let num2 = Math.floor((Math.random() * 10) + 1);
 let answer = num1 + num2;   
 
-rl.question(`What is ${ num1 } + ${ num2 }?`,
+rl.question(`What is ${ num1 } + ${ num2 }? /n`,
 (userInput)=>{
     if(userInput.trim() == answer){
         rl.close();
+    }
+    else{
+      rl.setPrompt('Incorrect response please try agin/n');
+      rl.prompt(); 
     }
 });
 
 rl.on('close', ()=>{
    console.log('Correct!!!!'); 
-})
+});
