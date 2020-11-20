c3ef85941ba7fc0157158ce447429927aebdfd8c
loy muriithi <loymur8gail.com>
2020-11-20 23:50:39 +0300

    commit

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
 //create a file
-fs.writeFile('example.txt',"this is an exmple",(err)=>{
+// fs.writeFile('example.txt',"this is an exmple",(err)=>{
+//     if(err)
+//     console.log(err);
+//     else{
+//         console.log('File Successfully created');
+//         fs.readFile('exmple.txt','utf8',(err,file)=>{
+//           if(err) 
+//           console.log(err); 
+//           else
+//           console.log(file)
+//         });
+//     }
+    
+// });
+
+fs.rename('example.txt', 'example2.txt',(err)=>{
     if(err)
     console.log(err);
-    else{
-        console.log('File Successfully created');
-        fs.readFile('exmple.txt','utf8',(err,file)=>{
-          if(err) 
-          console.log(err); 
-          else
-          console.log(file)
-        });
-    }
-    
-});
+    else
+    console.log('seccessfully renamed the file');
+})
