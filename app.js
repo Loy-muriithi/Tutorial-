f9321b820d79b0991841e1d76fbb4d35e8ebb920
loy muriithi <loymur8gail.com>
2020-11-21 02:09:12 +0300

    commit

--- app.js
+++ app.js
@@ -1,41 +1,6 @@
-const fs = require('fs');
-// fs.mkdir('tutorial',(err)=>{
-//     if(err)
-//         console.log(err);
-//         else{
-//        fs.writeFile('./tutorial/example.txt','123',(err)=>{
-//            if(err)
-//            console.log(err);
-//            else{
-//                console.log('successfully created file');
-//            }
-//        })
-//         }
-// })
-
-// fs.unlink('./tutorial/example.txt',(err)=>{
-//     if(err)
-//     console.log(err);
-//     else{
-//         fs.rmdir('tutorial',(err)=>{
-//         else{
-//             console.log('deleted folder');
-//         }
-
-    
-// })
-fs.readdir('example',(err,files)=>{
-   if(err)
-   console.log(err);
-   else{
-      for(let file of files){
-         fs.unlink('./example/' + file,(err)=>{
-             if(err)
-             console.log(err);
-             else{
-                 console.log('succusselly deleted file');
-             }
-         }) 
-      }
-   }
+const http = require('https');
+const server = http.createServer((req,res)=>{
+    res.write('Hello World from nodejs');
+    res.end();
 });
+server. listen('3000');
