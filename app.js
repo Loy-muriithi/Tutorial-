b5c7e8ee3b4bab5e34e235e0faf491c34a5b9bb3
loy muriithi <loymur8gail.com>
2020-11-21 22:49:02 +0300

    comiit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt','utf8');
+const readStream = fs.createReadStream('./eample,txt','utf8');
 const writeStream = fs.createWriteStream('exmple2.txt');
-readStream.pipe(writeStream);
+readStream.on('data',(chunk)=>{
+    writeStream.write(chunk);
+});
