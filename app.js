358c27c54566383c5dd4e081c0c068dba2a59f57
loy muriithi <loymur8gail.com>
2020-11-21 22:29:22 +0300

    commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
-const readStream = fs.createReadStream('./example.txt',)
+const readStream = fs.createReadStream('./example.txt','loy');
 const writeStream = fs.createWriteStream('exmple2.txt');
 readStream.pipe(writeStream);
