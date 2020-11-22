e0f05858315a53827467f4efee818caf03c68ca2
loy muriithi <loymur8gail.com>
2020-11-22 11:04:04 +0300

    commit

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const fs = require('fs');
 http.createServer((req,res)=>{
      const readstream = fs.createWriteStream('./static/index.html');
      res.writeHead(200,{'Content-type': 'text/html'});
+     readstream.pipe(res);
 }).listen(3000);
