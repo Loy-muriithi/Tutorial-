5784f519c0cef0a6234366ec0ca7bb3d724d3172
loy muriithi <loymur8gail.com>
2020-11-22 11:59:04 +0300

    commit

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-const http = require('http');
-const fs = require('fs');
-http.createServer((req,res)=>{
-     const readStream = fs.createReadStream('./static/example.json');
-     res.writeHead(200,{'Content-type': 'text/html'});
-     readStream.pipe(res);
-}).listen(3000);
+const_= require('lodash');
+let example = _.fill([1,2,3,4,5],"banana",1,4);
+console.log(example);
