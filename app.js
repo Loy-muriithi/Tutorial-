baf3e75526507d39c91c64b2f8e28d262ea1a8f8
loy muriithi <loymur8gail.com>
2020-11-24 10:54:08 +0300

    commit

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const _ = require('lodash');
-let example = _.fill([1,2,3,4,5],"banana",1,4);
-console.log(example);
+const express = require('express');
+const path = require('path');
+const Joi = require('joi');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/puplic',express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static','index.html'));
+});
+app.post('/',(req,res)=>{
+     console.log(req.body);
+     const schema = Joi.Object().keys({
+        email :  Joi.string().trim().email().required(),
+        password : Joi.string().min(5).max(15).required()
+     });
+     Joi.Validate(req.body,schema,(err,result)=>{
+         if(err){
+             console.log(err);
+             res.send('An error has occurred');
+         }
+         console.log(result);
+         res.send('Sucessfully posted data');
+     });
+    
+});
+app.listen(3000);
