5aa8eb67cf6de01a78f4b6b76a72853dabbfab13
loy muriithi <loymur8gail.com>
2020-11-26 23:53:37 +0300

    commit

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const path = require('path');
-const Joi = require('joi');
 const bodyParser = require('body-parser');
-const app = express();
-
-app.use('/puplic',express.static(path.join(__dirname,'static')));
+const app = express = express();
+app.use ('/public',express.static(path.join(__dirname,'static')));
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'static','index.html'));
+     res.sendFile(path.join(__dirname,'static','index.html'));
 });
+
 app.post('/',(req,res)=>{
-     console.log(req.body);
-     const schema = Joi.Object().keys({
-        email :  Joi.string().trim().email().required(),
-        password : Joi.string().min(5).max(15).required()
-     });
-     Joi.Validate(req.body,schema,(err,result)=>{
-         if(err){
-             console.log(err);
-             res.send('An error has occurred');
-         }
-         console.log(result);
-         res.send('Sucessfully posted data');
-     });
-    
+    console.log(req.body);
+    // database work her
+    res.send('successfully posted data');
 });
 app.listen(3000);
